82534c8e0a171393a47c7fedf14cbb3ebba5d231
David-V-Nguyen <[200~49809771+David-V-Nguyen@users.noreply.github.com>
2022-06-13 13:33:04 +0100

    Added new colours array to create circular version of colour picker palette, also installed new modules ngx-color to test out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ColorPickerModule } from 'ngx-color-picker';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ColourWheelComponent } from './colour-wheel/colour-wheel.component';
 import { ColoursComponent } from './colours/colours.component';
+
+import { ColorPickerModule } from 'ngx-color-picker';
+import { ColorSketchModule} from 'ngx-color/sketch';
+import { ColorCircleModule} from 'ngx-color/circle';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,8 @@ import { ColoursComponent } from './colours/colours.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ColorCircleModule,
+    ColorSketchModule,
     ColorPickerModule
   ],
   providers: [],
